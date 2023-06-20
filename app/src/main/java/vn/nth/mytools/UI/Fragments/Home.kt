33c8f668dffcf6a62cf1c193115fd40f93e37a7b
unknown <huy0363894103@gmail.com>
2023-06-20 20:26:49 +0700

    Design home fragment

--- app/src/main/java/vn/nth/mytools/UI/Fragments/Home.kt
+++ app/src/main/java/vn/nth/mytools/UI/Fragments/Home.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import vn.nth.mytools.App
 import vn.nth.mytools.databinding.FragmentHomeBinding
 
 class Home : Fragment(){
@@ -16,5 +17,14 @@ class Home : Fragment(){
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initEvent()
+    }
+
+    private fun initEvent() {
+
     }
 }
